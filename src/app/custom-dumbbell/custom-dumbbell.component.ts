c52594b6f6f49f09785a1d6a6e38b28b599e2cd5
ZubairPA <zubair.shaikh@port-ability.co.in>
2022-11-08 15:33:01 +0530

    dumbbell custom tooltip and plot color changes

--- src/app/custom-dumbbell/custom-dumbbell.component.ts
+++ src/app/custom-dumbbell/custom-dumbbell.component.ts
@@ -13,67 +13,31 @@ export class CustomDumbbellComponent implements OnInit {
       "make": "JEEP",
       "model": "WRANGLER",
       "make_model": "JEEP WRANGLER",
-      "time": "30",
-      "num_vehicles_t0": 25,
-      "num_vehicles_t": "8",
-      "num_vehicles_t1": "12",
-      "num_vehicles_t2": "17",
-      "surv_prob": "0.53"
+      "survey_items": [60, 20, 40, 80]
     },
     {
       "make": "JEEP",
       "model": "WRANGLER UNLIMITED",
       "make_model": "JEEP WRANGLER UNLIMITED",
-      "time": "30",
-      "num_vehicles_t0": 55,
-      "num_vehicles_t": "23",
-      "num_vehicles_t1": "40",
-      "num_vehicles_t2": "50",
-      "surv_prob": "0.43"
+      "survey_items": [50, 85, 40, 100]
     },
     {
       "make": "JEEP",
       "model": "RENEGADE",
       "make_model": "JEEP RENEGADE",
-      "time": "30",
-      "num_vehicles_t0": 59,
-      "num_vehicles_t": "39",
-      "num_vehicles_t1": "50",
-      "num_vehicles_t2": "56",
-      "surv_prob": "0.67"
-    },
-    {
-      "make": "JEEP",
-      "model": "COMPASS",
-      "make_model": "JEEP COMPASS",
-      "time": "30",
-      "num_vehicles_t0": 60,
-      "num_vehicles_t": "35",
-      "num_vehicles_t1": "50",
-      "num_vehicles_t2": "53",
-      "surv_prob": "0.59"
+      "survey_items": [89, 60, 78, 140]
     },
     {
       "make": "JEEP",
       "model": "CHEROKEE",
       "make_model": "JEEP CHEROKEE",
-      "time": "30",
-      "num_vehicles_t0": 124,
-      "num_vehicles_t": "80",
-      "num_vehicles_t1": "90",
-      "num_vehicles_t2": "100",
-      "surv_prob": "0.65"
+      "survey_items": [40, 60, 80, 110]
     },
     {
       "make": "JEEP",
       "model": "GRAND CHEROKEE",
       "make_model": "JEEP GRAND CHEROKEE",
-      "time": "30",
-      "num_vehicles_t0": "160",
-      "num_vehicles_t": "83",
-      "num_vehicles_t1": "95",
-      "num_vehicles_t2": "100",
-      "surv_prob": "0.62"
+      "survey_items": [46, 15, 78, 105]
     }
   ];
 
@@ -95,13 +59,11 @@ export class CustomDumbbellComponent implements OnInit {
     let y = d3.scalePoint().rangeRound([height, 10]).padding(0.4);
 
     let chart = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    // sort data
-    data.sort(function (a: any, b: any) {
-      return d3.ascending(+a.num_vehicles_t0, +b.num_vehicles_t0);
-    });
 
-    x.domain([0, (d3 as any).max(data, function (d: any) { return d.num_vehicles_t0; })]);
-    y.domain(data.map(function (d: any) { return d.make_model; }));
+    x.domain([0, 180]);
+    y.domain(data.map(function (d: any) {
+      return d.make_model;
+    }));
 
     // x-axis
     chart.append("g")
@@ -129,28 +91,33 @@ export class CustomDumbbellComponent implements OnInit {
     // **** Tooltip ZS ***
     var tooltip = d3.select("body").append("div").attr('class', 'tooltip');
     dumbbell.attr("cx", function (d: any, i: any) {
-      console.log(d, "Zub")
       return (i * 50) + 25;
     }).attr("cy", 150 / 2).attr("r", function (d: any) {
       return d;
-    }).on("mouseover", function (d: any) {
-      console.log(d)
-      return tooltip.style("visibility", "visible").text('Value: ' + d.target.__data__.num_vehicles_t0);
-    })
-      // we move tooltip during of "mousemove"
-      .on("mousemove", function (event: any) {
-        return tooltip.style("top", (event.pageY - 30) + "px").style("left", event.pageX + "px");
+    }).on("click", function (d: any) {
+      return tooltip.style("visibility", "visible").text('Value: ' + d.target.__data__)
+      .append('span').attr("class", "close").html('&times').on("click", function() {
+        tooltip.style("visibility", "hidden")
       })
-      // we hide our tooltip on "mouseout"
-      .on("mouseout", function () {
-        return tooltip.style("visibility", "hidden");
-      });
+    }).on("mousemove", function (event: any) {
+      return tooltip.style("top", (event.pageY - 30) + "px").style("left", event.pageX + 5 + "px");
+    })
+    // we hide our tooltip on "mouseout"
+    .on("mouseout", function () {
+      //return tooltip.style("visibility", "hidden");
+    });
 
     // lines: between dots
     dumbbell.append("line")
       .attr("class", "line between")
-      .attr("x1", function (d: any) { return x(d.num_vehicles_t); })
-      .attr("x2", function (d: any) { return x(d.num_vehicles_t0); })
+      .attr("x1", function (d: any) {
+        var min = Math.min(...d.survey_items)
+        return x(min);
+      })
+      .attr("x2", function (d: any) {
+        var max = Math.max(...d.survey_items)
+        return x(max);
+      })
       .attr("y1", 0)
       .attr("y2", 0);
 
@@ -162,68 +129,31 @@ export class CustomDumbbellComponent implements OnInit {
     //   .attr("y1", 0)
     //   .attr("y2", 0);
 
+    var colors = ["#ff8d00", "#12a599", "blue", "#9024d5"];
     // dots: current inventory
-    dumbbell.append("circle")
+    dumbbell.append("g").selectAll("circle")
+      .data(function (d: any) { return d.survey_items })
+      .enter()
+      .append("circle")
+      .style("fill", function(d:any, i:any) { return colors[i] })
       .attr("class", "circle current")
-      .attr("cx", function (d: any) { return x(d.num_vehicles_t0); })
-      .attr("cy", 0)
-      .attr("r", 5);
+      .attr("r", 5)
+      .attr("cx", function (dd: any) { return x(dd) })
+      .attr("cy", 0);
 
-    // data labels: current
-    dumbbell.append("text")
+    dumbbell.append("g").selectAll("text")
+      .data(function (d: any) { return d.survey_items })
+      .enter()
+      .append("text").text(function (d: any) {
+        return d;
+      })
       .attr("class", "text current")
-      .attr("x", function (d: any) { return x(d.num_vehicles_t0); })
+      .attr("x", function (d: any) {
+        return x(d);
+      })
       .attr("y", 0)
-      .attr("dy", ".35em")
-      .attr("dx", 10)
-      .text(function (d: any) { return d.num_vehicles_t0; });
-
-    // data labels: future
-    dumbbell.append("text")
-      .attr("class", "text future")
-      .attr("x", function (d: any) { return x(d.num_vehicles_t); })
-      .attr("y", 0)
-      .attr("dy", ".35em")
-      .attr("dx", -10)
-      .attr("text-anchor", "end")
-      .text(function (d: any) { return d.num_vehicles_t; });
-
-
-    // d3.select(".dumbbell:last-child")
-    //   .append("text")
-    //   .attr("class", "label current")
-    //   .attr("x", function (d: any) { return x(d.num_vehicles_t0); })
-    //   .attr("y", 0)
-    //   .attr("dy", -20)
-    //   .attr("text-anchor", "middle")
-    //   .text("Current");
-    // d3.select(".dumbbell:last-child")
-    //   .append("text")
-    //   .attr("class", "label future")
-    //   .attr("x", function (d: any) { return x(d.num_vehicles_t); })
-    //   .attr("y", 0)
-    //   .attr("dy", -20)
-    //   .attr("text-anchor", "middle")
-    //   .text(function (d: any) { return d.time + " Days"; });
-
-    // dots: future inventory
-    dumbbell.append("circle")
-      .attr("class", "circle future")
-      .attr("cx", function (d: any) { return x(d.num_vehicles_t); })
-      .attr("cy", 0)
-      .attr("r", 5);
-
-    dumbbell.append("circle")
-      .attr("class", "circle future")
-      .attr("cx", function (d: any) { return x(d.num_vehicles_t1); })
-      .attr("cy", 0)
-      .attr("r", 5);
-
-    dumbbell.append("circle")
-      .attr("class", "circle future")
-      .attr("cx", function (d: any) { return x(d.num_vehicles_t2); })
-      .attr("cy", 0)
-      .attr("r", 5);
+      .attr("dy", "1.6em")
+      .attr("dx", -6);
   }
 
 }
